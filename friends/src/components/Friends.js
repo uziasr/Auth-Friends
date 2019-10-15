601845cc742fbcaab57e90c5661594583050b4eb
Uzias Rivera <uziasrivera@gmail.com>
2019-10-15 15:03:07 -0700

    rendered friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const Friends = (props) => {
-            
+    const [friends, setFriends] = useState([])
+    console.log(props)
+    const getData = () => {
+        // fetch data from the server
+        // the data is protected behind a token
+        // so our request needs to include an `Authorization: token` header
+        // TODO: Fetch this data - `/api/data` - and add the array of gas prices to state
+        axiosWithAuth()
+          .get('/api/friends')
+          .then(res => setFriends(...friends,res.data))
+          .catch(err => console.log(err.response));}
+    useEffect(()=>{
+        if(friends.length===0){
+            console.log('hello')
+            getData()
+      } },[])
+     console.log(friends)
     return(
-        <div>e</div>
+        friends.map(friend=>{
+            return(
+                <div>
+                    <h1>{friend.name}</h1>
+                    <p>{friend.age}</p>
+                    <p>{friend.email}</p>
+                </div>
+                )
+            }
+        )
     )
 }
 
